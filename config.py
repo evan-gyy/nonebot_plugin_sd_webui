226df236bd9fd3819eae2024fe4ac2e1a531ab16
evan-gyy <2420298062@qq.com>
2023-05-05 18:32:34 +0800

    initial commit

---
+++ config.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from pydantic import BaseModel, Extra
+from nonebot import get_driver
+from pathlib import Path
+
+
+class Config(BaseModel, extra=Extra.ignore):
+    # sd
+    api_host = '127.0.0.1'
+    api_port = '7860'
+    save_path = Path(__file__).parent / "save_images"
+    negative_prompt = 'ugly, nsfw, lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry'
+    sampler = 'DPM++ 2M'
+    steps = 20
+
+
+driver = get_driver()
+config = Config.parse_obj(driver.config)
